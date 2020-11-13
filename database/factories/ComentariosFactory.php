d76631b4996003f69913a84fd7ccb803dd562b97
Alex Lozano <19170025@uttcampus.edu.mx>
2020-11-12 20:51:18 -0600

    Inicio del proyecto

---
+++ database/factories/ComentariosFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\comentarios;
+use Faker\Generator as Faker;
+
+$factory->define(comentarios::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
